29e0e11c4948153fd0edca68191075c26983a197
Greg Johnston <gcjohnston91@gmail.com>
2020-06-17 12:57:00 -0400

    Pulled links into new div

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,29 +11,31 @@ function Portfolio(props) {
   // }
 
   return (
-    <div className={props.isVisible ? "mi-portfolio mi-portfolio-visible" : "mi-portfolio"}>
-      <div className="mi-portfolio-image">
-        <img src={imageUrl} alt={title}/>
-        <ul>
-          <p>Front End</p>
-          <p>{technologies}</p>
-          <p>Back End</p>
-          <div class='api-links'>
-            {apis.map((api) => <a href={api} target='_blank' key={api}>{api}</a>)}
-          </div>
-        </ul>
+    <div>
+      <div className={props.isVisible ? "mi-portfolio mi-portfolio-visible" : "mi-portfolio"}>
+        <div className="mi-portfolio-image">
+          <img src={imageUrl} alt={title}/>
+          <ul>
+            <p>Front End</p>
+            <p>{technologies}</p>
+            <p>Back End</p>
+            <div class='api-links'>
+              {apis.map((api) => <a href={api} target='_blank' key={api}>{api}</a>)}
+            </div>
+          </ul>
+        </div>
+        {!url ? <h5>{title}</h5> : <h5>
+          <a rel="noopener noreferrer" target="_blank" href={url}>
+            {title}
+          </a>
+        </h5>}
+        {subtitle ? <h6>{subtitle}</h6> : null}
+        {!largeImageUrl ? null : <FsLightbox
+          // toggler={toggler}
+          sources={largeImageUrl}
+        />
+        }
       </div>
-      {!url ? <h5>{title}</h5> : <h5>
-        <a rel="noopener noreferrer" target="_blank" href={url}>
-          {title}
-        </a>
-      </h5>}
-      {subtitle ? <h6>{subtitle}</h6> : null}
-      {!largeImageUrl ? null : <FsLightbox
-        // toggler={toggler}
-        sources={largeImageUrl}
-      />
-      }
       <div className='portfolio-links'>
         <a rel="noopener noreferrer" target="_blank" href={url}>
           <Icon.Link/>
@@ -43,7 +45,7 @@ function Portfolio(props) {
         </a>
       </div>
     </div>
-  );
+  )
 }
 
 export default Portfolio;
